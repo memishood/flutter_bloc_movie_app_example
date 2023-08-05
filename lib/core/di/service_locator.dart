534553d3ff0a34e589486f7f764f665a699e768c
Emre Memis <emre.memis.49@gmail.com>
2023-08-05 22:20:04 +0300

    create datasources for movies page

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc_movie_app_example/core/api/api_client.dart';
 import 'package:flutter_bloc_movie_app_example/core/route/app_router.dart';
+import 'package:flutter_bloc_movie_app_example/features/movies/data/datasources/movies_remote_data_source_impl.dart';
+import 'package:flutter_bloc_movie_app_example/features/movies/domain/datasources/movies_remote_data_source.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 
@@ -8,6 +10,7 @@ final getIt = GetIt.instance;
 
 void setUp() {
   setUpCommons();
+  setUpMoviesPage();
 }
 
 void setUpCommons() {
@@ -16,4 +19,10 @@ void setUpCommons() {
     ..registerSingleton<Logger>(Logger())
     ..registerSingleton<Dio>(Dio())
     ..registerSingleton<ApiClient>(ApiClient(getIt()));
+}
+
+void setUpMoviesPage() {
+  getIt.registerFactory<MoviesRemoteDataSource>(
+    () => MoviesRemoteDataSourcesImpl(getIt()),
+  );
 }
---
+++ lib/features/movies/data/datasources/movies_remote_data_source_impl.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_bloc_movie_app_example/core/api/api_client.dart';
+import 'package:flutter_bloc_movie_app_example/features/movies/domain/datasources/movies_remote_data_source.dart';
+import 'package:flutter_bloc_movie_app_example/features/movies/domain/entities/movies_entity.dart';
+
+class MoviesRemoteDataSourcesImpl implements MoviesRemoteDataSource {
+  const MoviesRemoteDataSourcesImpl(this._apiClient);
+  final ApiClient _apiClient;
+
+  @override
+  Future<MoviesEntity> getLatestMovies() {
+    return _apiClient.getMovies(MovieType.latest);
+  }
+
+  @override
+  Future<MoviesEntity> getPopularMovies() {
+    return _apiClient.getMovies(MovieType.popular);
+  }
+
+  @override
+  Future<MoviesEntity> getTopRatedMovies() {
+    return _apiClient.getMovies(MovieType.topRated);
+  }
+
+  @override
+  Future<MoviesEntity> getUpcomingMovies() {
+    return _apiClient.getMovies(MovieType.upcoming);
+  }
+}
